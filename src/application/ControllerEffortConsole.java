4551cdf3973297f775eb4209c6c52c36c00e3180
nsripapa <nsripapa@asu.edu>
2023-10-26 04:00:08 -0700

    GUI for Log Editor
    NOT FUNCTIONING YET!!!
    
    Added GUI elements for the Log Editor page.

--- src/application/ControllerEffortConsole.java
+++ src/application/ControllerEffortConsole.java
@@ -4,43 +4,50 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 public class ControllerEffortConsole extends Controller implements Initializable 
 {
 	LocalTime startTime, endTime; int elapsedMinutes, elapsedSeconds, elapsedHours;
 	LocalDate date;
+	
 	@FXML
 	private ControllerMenuBar menuBarController;
 	
+	@FXML
+	private Label timerLabel;
 	
+	
+	DateTimeFormatter form = DateTimeFormatter.ofPattern("HH:mm:ss"); // Time format
 	
 	public void startActivity(ActionEvent e) {
 		startTime = java.time.LocalTime.now();
 		date = java.time.LocalDate.now();
 		
-		System.out.println("Activity started at time: " + startTime.toString() + " on " + date.toString());
+		timerLabel.setText("Activity started at time: " + startTime.format(form) + " on " + date.toString()); 
 	}
 	
 	
 	public void endActivity(ActionEvent e) {
+		
 		if(startTime != null) {
 			
 		endTime = java.time.LocalTime.now();
-		elapsedSeconds = endTime.getSecond() - startTime.getSecond();
-		elapsedMinutes = endTime.getMinute() - startTime.getMinute();
-		elapsedHours = endTime.getHour() - startTime.getHour();
 		
-		System.out.println(elapsedHours + ":" + elapsedMinutes + ":" + elapsedSeconds);
+		timerLabel.setText("Activity ended at time: " + endTime.format(form));
 		
-		System.out.println(date.toString());
 		}
 	}
 	
 	
+
 	
 	
 	
