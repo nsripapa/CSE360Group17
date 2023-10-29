727d9e41e9f2d2526f08fd3fe989c1f18de96df0
Ribhay27 <148173934+Ribhay27@users.noreply.github.com>
2023-10-29 14:21:46 -0700

    Update EffortLoggerSimulator.java

--- src/application/EffortLoggerSimulator.java
+++ src/application/EffortLoggerSimulator.java
@@ -1,24 +1,38 @@
 package application;
-
+//Author: Ribhay Singh
+//This is the basic layout of how the test cases will be carried out. 
+//For the sake of reference and simplicity, I have decided to test the UserName and Password function of the program 
 public class EffortLoggerSimulator {
 
     private UNandPW usersAndPasswords = new UNandPW();
-
+//In application, we will add more test cases.
     public String runTestCases() {
         StringBuilder results = new StringBuilder();
 
-        results.append(testUserLogin());
-        results.append(testAddEntry());
-        results.append(testDeleteEntry());
-        results.append(testUpdateEntry());
-        results.append(testRetrieveEntry());
+        results.append(testUNandPWInitialization());
+        results.append(testGetLoginInfo());
 
         return results.toString();
     }
 
-  
-    private String testUserLogin() {
-        boolean success = effortLogger.login("testUser", "testPassword");
-        return success ? "User Login Test: Success\n" : "User Login Test: Failed\n";
+    private String testUNandPWInitialization() {
+        // Check if the HashMap contains the hardcoded keys
+        boolean containsKeyA = usersAndPasswords.getLoginInfo().containsKey("a");
+        boolean containsKeyJfett2 = usersAndPasswords.getLoginInfo().containsKey("jfett2");
+        boolean containsKeyNsripapa3 = usersAndPasswords.getLoginInfo().containsKey("nsripapa3");
+        boolean containsKeyRsingh7 = usersAndPasswords.getLoginInfo().containsKey("rsingh7");
+        
+        return (containsKeyA && containsKeyJfett2 && containsKeyNsripapa3 && containsKeyRsingh7) 
+            ? "UNandPW Initialization Test: Success\n" 
+            : "UNandPW Initialization Test: Failed\n";
     }
-//More to be added 
+
+    private String testGetLoginInfo() {
+        // Simply check if getLoginInfo() returns a non-empty HashMap
+        boolean hasInfo = !usersAndPasswords.getLoginInfo().isEmpty();
+
+        return hasInfo 
+            ? "Get Login Info Test: Success\n" 
+            : "Get Login Info Test: Failed\n";
+    }
+}
