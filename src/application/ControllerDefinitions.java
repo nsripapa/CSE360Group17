f36faabed15e1ca1723864dfbb22898f35436087
Jacob Fett <93690458+jacobfett03@users.noreply.github.com>
2023-11-11 12:26:26 -0700

    Added predefined values to definitions tab

--- src/application/ControllerDefinitions.java
+++ src/application/ControllerDefinitions.java
@@ -5,11 +5,13 @@ import java.util.ResourceBundle;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
@@ -24,69 +26,260 @@ public class ControllerDefinitions extends Controller implements Initializable
 	buttonAddChoiceBox1, buttonDeleteChoiceBox1, buttonAddChoiceBox2, buttonDeleteChoiceBox2;
 	@FXML
 	private TextField textField;
+	@FXML
+	private Label labelName, labelChoiceBox1, labelChoiceBox2;
 	
 	private String[] sections = {"Projects", "Life Cycle Steps", "Effort Categories", 
 			"Plans", "Deliverables", "Interruptions", "Defect Categories"};
 	
 	private Definitions definitions = new Definitions();
 	
-	private void setChoiceBoxList(String newValue) 
+	private void setChoiceBoxList(String section)
 	{
-		switch (newValue)
+		choiceBoxList.getItems().clear();
+		
+		switch (section)
 		{
 		case ("Projects"):
 			if (definitions.projects != null)
 			{
-				for (Project project : definitions.projects)
-				{
-					choiceBoxList.getItems().add(project.name);
-				}
+					
+				choiceBoxList.setItems(FXCollections.observableArrayList(definitions.getProjectNames()));
+				
+				labelChoiceBox1.setText("Life Cycle Steps for Project");
+				labelChoiceBox1.setVisible(true);
+				labelChoiceBox2.setVisible(false);
 			}
+			break;
+		case ("Effort Categories"):
+			if (definitions.effortCategories != null)
+			{
+				choiceBoxList.setItems(FXCollections.observableArrayList(definitions.getEffortCategoryNames()));
+				labelChoiceBox1.setVisible(false);
+				labelChoiceBox2.setVisible(false);
+			}
+			break;
 		case ("Life Cycle Steps"):
 			if (definitions.lifeCycleSteps != null)
 			{
-				for (LifeCycleStep lifeCycleStep : definitions.lifeCycleSteps)
-				{
-					choiceBoxList.getItems().add(lifeCycleStep.name);
-				}
+				choiceBoxList.setItems(FXCollections.observableArrayList(definitions.getLifeCycleStepNames()));
+				
+				labelChoiceBox1.setText("Effort Category Default");
+				labelChoiceBox1.setVisible(true);
+				labelChoiceBox2.setText("Deliverable Default");
+				labelChoiceBox2.setVisible(true);
 			}
+			break;	
 		case ("Plans"):
-			if (definitions.effortCategories != null)
+			if (definitions.plans != null)
 			{
-				for (EffortCategory effortCategory : definitions.effortCategories)
-				{
-					choiceBoxList.getItems().add(effortCategory.name);
-				}
+				choiceBoxList.setItems(FXCollections.observableArrayList(definitions.getPlanNames()));
+				labelChoiceBox1.setVisible(false);
+				labelChoiceBox2.setVisible(false);
 			}
+			break;
 		case ("Deliverables"):
-			if (definitions.effortCategories != null)
+			if (definitions.deliverables != null)
 			{
-				for (EffortCategory effortCategory : definitions.effortCategories)
-				{
-					choiceBoxList.getItems().add(effortCategory.name);
-				}
+				choiceBoxList.setItems(FXCollections.observableArrayList(definitions.getDeliverableNames()));
+				labelChoiceBox1.setVisible(false);
+				labelChoiceBox2.setVisible(false);
 			}
+			break;
 		case ("Interruptions"):
-			if (definitions.effortCategories != null)
+			if (definitions.interruptions != null)
 			{
-				for (EffortCategory effortCategory : definitions.effortCategories)
-				{
-					choiceBoxList.getItems().add(effortCategory.name);
-				}
+				choiceBoxList.setItems(FXCollections.observableArrayList(definitions.getInterruptionNames()));
+				labelChoiceBox1.setVisible(false);
+				labelChoiceBox2.setVisible(false);
 			}
+			break;
 		case ("Defect Categories"):
-			if (definitions.effortCategories != null)
+			if (definitions.defectCategories != null)
 			{
-				for (EffortCategory effortCategory : definitions.effortCategories)
-				{
-					choiceBoxList.getItems().add(effortCategory.name);
-				}
+				choiceBoxList.setItems(FXCollections.observableArrayList(definitions.getDefectCategoryNames()));
+				labelChoiceBox1.setVisible(false);
+				labelChoiceBox2.setVisible(false);
 			}
+			break;
 		}
 		
 	}
 	
+	public void addListItem(MouseEvent e)
+	{
+		
+		String name = textField.getText();
+		textField.clear();
+		
+		if (name.length() == 0) return;
+		
+		switch(choiceBoxSection.getValue())
+		{
+		
+		case ("Projects"):
+			
+			Project project = new Project();
+			project.name = name;
+			definitions.projects.add(project);
+			setChoiceBoxList("Projects");
+			break;
+			
+		case ("Life Cycle Steps"):
+			
+			LifeCycleStep lifeCycleStep = new LifeCycleStep();
+			lifeCycleStep.name = name;
+			definitions.lifeCycleSteps.add(lifeCycleStep);
+			setChoiceBoxList("Life Cycle Steps");
+			break;
+			
+		case ("Effort Categories"):
+			
+			EffortCategory effortCategory = new EffortCategory();
+			effortCategory.name = name;
+			definitions.effortCategories.add(effortCategory);
+			setChoiceBoxList("Effort Categories");
+			break;
+			
+		case ("Plans"):
+			
+			Plan plan = new Plan();
+			plan.name = name;
+			definitions.plans.add(plan);
+			setChoiceBoxList("Plans");
+			break;
+			
+		case ("Deliverables"):
+			
+			Deliverable deliverable = new Deliverable();
+			deliverable.name = name;
+			definitions.deliverables.add(deliverable);
+			setChoiceBoxList("Deliverables");
+			break;
+			
+		case ("Interruptions"):
+			
+			Interruption interruption = new Interruption();
+			interruption.name = name;
+			definitions.interruptions.add(interruption);
+			setChoiceBoxList("Interruptions");
+			break;
+			
+		case ("Defect Categories"):
+			
+			DefectCategory defectCategory = new DefectCategory();
+			defectCategory.name = name;
+			definitions.defectCategories.add(defectCategory);
+			setChoiceBoxList("Defect Categories");
+			break;
+			
+		}
+		
+	}
 	
+	public void deleteListItem(MouseEvent e)
+	{
+		String name = choiceBoxList.getValue();
+		textField.clear();
+		
+		if (name.length() == 0) return;
+		
+		switch(choiceBoxSection.getValue())
+		{
+		
+		case ("Projects"):
+			
+			for (Project project : definitions.projects)
+			{
+				if (project.name == name)
+				{
+					definitions.projects.remove(project);
+					break;
+				}
+			}
+			setChoiceBoxList("Projects");
+			break;
+			
+		case ("Life Cycle Steps"):
+			
+			for (LifeCycleStep lcs : definitions.lifeCycleSteps)
+			{
+				if (lcs.name == name)
+				{
+					definitions.lifeCycleSteps.remove(lcs);
+					break;
+				}
+			}
+			setChoiceBoxList("Life Cycle Steps");
+			break;
+			
+		case ("Effort Categories"):
+			
+			for (EffortCategory ec : definitions.effortCategories)
+			{
+				if (ec.name == name)
+				{
+					definitions.effortCategories.remove(ec);
+					break;
+				}
+			}
+			setChoiceBoxList("Effort Categories");
+			break;
+			
+		case ("Plans"):
+			
+			for (Plan plan : definitions.plans)
+			{
+				if (plan.name == name)
+				{
+					definitions.plans.remove(plan);
+					break;
+				}
+			}
+			setChoiceBoxList("Plans");	
+			break;
+			
+		case ("Deliverables"):
+			
+			for (Deliverable deliverable : definitions.deliverables)
+			{
+				if (deliverable.name == name)
+				{
+					definitions.deliverables.remove(deliverable);
+					break;
+				}
+			}
+			setChoiceBoxList("Deliverables");
+			break;
+			
+		case ("Interruptions"):
+			
+			for (Interruption interruption : definitions.interruptions)
+			{
+				if (interruption.name == name)
+				{
+					definitions.interruptions.remove(interruption);
+					break;
+				}
+			}
+			setChoiceBoxList("Interruptions");
+			break;
+			
+		case ("Defect Categories"):
+			
+			for (DefectCategory dc : definitions.defectCategories)
+			{
+				if (dc.name == name)
+				{
+					definitions.defectCategories.remove(dc);
+					break;
+				}
+			}
+			setChoiceBoxList("Defect Categories");
+			break;
+			
+		}
+	}
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
@@ -97,19 +290,9 @@ public class ControllerDefinitions extends Controller implements Initializable
 		//add the sections to the first ChocieBox
 		choiceBoxSection.getItems().addAll(sections);
 		
-		//listen for when the first ChoiceBox changes
-		choiceBoxSection.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>()
-		{
-
-			@Override
-			public void changed(ObservableValue<? extends String> arg0, String oldValue, String newValue) {
-				// TODO Auto-generated method stub
-
-				//set the options for the 2nd ChoiceBox
-				setChoiceBoxList(newValue);
-			}
-
-			
+		choiceBoxSection.setOnAction(e -> {
+			String section = choiceBoxSection.getValue();
+			setChoiceBoxList(section);
 		});
 	}
 }
--- src/application/Definitions.java
+++ src/application/Definitions.java
@@ -1,18 +1,373 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Definitions 
 {
 	Definitions()
 	{
-		projects = null;;
-		lifeCycleSteps = null;
-		effortCategories = null;
-		plans = null;
-		deliverables = null;
-		interruptions = null;
-		defectCategories = null;
+		projects = new ArrayList<>();
+		lifeCycleSteps = new ArrayList<>();
+		effortCategories = new ArrayList<>();
+		plans = new ArrayList<>();
+		deliverables = new ArrayList<>();
+		interruptions = new ArrayList<>();
+		defectCategories = new ArrayList<>();
+		
+		
+		//predefined values
+		
+		//PLANS
+		Plan plan = new Plan();
+		
+		plan.name = "Project Plan";
+		plans.add(plan);
+		
+		plan.name = "Risk Management Plan";
+		plans.add(plan);
+		
+		plan.name = "Conceptual Design Plan";
+		plans.add(plan);
+		
+		plan.name = "Detailed Design Plan";
+		plans.add(plan);
+		
+		plan.name = "Implementation Plan";
+		plans.add(plan);
+		
+		//DELIVERABLES
+		Deliverable deliverable = new Deliverable();
+		
+		deliverable.name = "Conceptual Design";
+		deliverables.add(deliverable);
+		
+		deliverable = new Deliverable();
+		deliverable.name = "Detailed Design";
+		deliverables.add(deliverable);
+		
+		deliverable = new Deliverable();
+		deliverable.name = "Test Cases";
+		deliverables.add(deliverable);
+		
+		deliverable = new Deliverable();
+		deliverable.name = "Solution";
+		deliverables.add(deliverable);
+		
+		deliverable = new Deliverable();
+		deliverable.name = "Reflection";
+		deliverables.add(deliverable);
+		
+		deliverable = new Deliverable();
+		deliverable.name = "Outline";
+		deliverables.add(deliverable);
+
+		deliverable = new Deliverable();
+		deliverable.name = "Draft";
+		deliverables.add(deliverable);
+
+		deliverable = new Deliverable();
+		deliverable.name = "Report";
+		deliverables.add(deliverable);
+
+		deliverable = new Deliverable();
+		deliverable.name = "User Defined";
+		deliverables.add(deliverable);
+
+		deliverable = new Deliverable();
+		deliverable.name = "Other";
+		deliverables.add(deliverable);
+		
+		//INTERRUPTIONS
+		Interruption interruption = new Interruption();
+		
+		interruption.name = "Break";
+		interruptions.add(interruption);
+		
+		interruption = new Interruption();
+		interruption.name = "Phone";
+		interruptions.add(interruption);
+
+		interruption = new Interruption();
+		interruption.name = "Teammate";
+		interruptions.add(interruption);
+
+		interruption = new Interruption();
+		interruption.name = "Visitor";
+		interruptions.add(interruption);
+
+		interruption = new Interruption();
+		interruption.name = "Other";
+		interruptions.add(interruption);
+		
+		
+		//DEFECT CATEGORIES
+		DefectCategory dc = new DefectCategory();
+		
+		dc.name = "Not Specified";
+		defectCategories.add(dc);
+		
+		dc = new DefectCategory();
+		dc.name = "Documentation";
+		defectCategories.add(dc);
+
+		dc = new DefectCategory();
+		dc.name = "Syntax";
+		defectCategories.add(dc);
+
+		dc = new DefectCategory();
+		dc.name = "Build, Package";
+		defectCategories.add(dc);
+
+		dc = new DefectCategory();
+		dc.name = "Assignment";
+		defectCategories.add(dc);
+
+		dc = new DefectCategory();
+		dc.name = "Interface";
+		defectCategories.add(dc);
+
+		dc = new DefectCategory();
+		dc.name = "Checking";
+		defectCategories.add(dc);
+
+		dc = new DefectCategory();
+		dc.name = "Data";
+		defectCategories.add(dc);
+
+		dc = new DefectCategory();
+		dc.name = "Function";
+		defectCategories.add(dc);
+
+		dc = new DefectCategory();
+		dc.name = "System";
+		defectCategories.add(dc);
+
+		dc = new DefectCategory();
+		dc.name = "Environment";
+		defectCategories.add(dc);
+		
+		//EFFORT CATEGORIES
+		EffortCategory ec = new EffortCategory();
+		
+		ec.name = "Plans";
+		effortCategories.add(ec);
+		
+		ec = new EffortCategory();
+		ec.name = "Deliverables";
+		effortCategories.add(ec);
+
+		ec = new EffortCategory();
+		ec.name = "Interruptions";
+		effortCategories.add(ec);
+
+		ec = new EffortCategory();
+		ec.name = "Defects";
+		effortCategories.add(ec);
+
+		ec = new EffortCategory();
+		ec.name = "Others";
+		effortCategories.add(ec);
+		
+		//LIFE CYCLE STEPS
+		LifeCycleStep lcs = new LifeCycleStep();
+		
+		lcs.name = "Problem Understanding";
+		lcs.defaultEffortCategory = lcs.getEffortCategory(effortCategories, "Deliverables");
+		lcs.defaultDeliverable = lcs.getDeliverable(deliverables, "Conceptual Design");
+		lifeCycleSteps.add(lcs);
+		
+		lcs = new LifeCycleStep();
+		lcs.name = "Conceptual Design Plan";
+		lcs.defaultEffortCategory = lcs.getEffortCategory(effortCategories, "Plans");
+		lcs.defaultDeliverable = lcs.getDeliverable(deliverables, "Test Cases");
+		lifeCycleSteps.add(lcs);
+
+		lcs = new LifeCycleStep();
+		lcs.name = "Requirements";
+		lcs.defaultEffortCategory = lcs.getEffortCategory(effortCategories, "Deliverables");
+		lcs.defaultDeliverable = lcs.getDeliverable(deliverables, "Conceptual Design");
+		lifeCycleSteps.add(lcs);
+
+		lcs = new LifeCycleStep();
+		lcs.name = "Conceptual Design";
+		lcs.defaultEffortCategory = lcs.getEffortCategory(effortCategories, "Deliverables");
+		lcs.defaultDeliverable = lcs.getDeliverable(deliverables, "Conceptual Design");
+		lifeCycleSteps.add(lcs);
+
+		lcs = new LifeCycleStep();
+		lcs.name = "Conceptual Design Review";
+		lcs.defaultEffortCategory = lcs.getEffortCategory(effortCategories, "Deliverables");
+		lcs.defaultDeliverable = lcs.getDeliverable(deliverables, "Conceptual Design");
+		lifeCycleSteps.add(lcs);
+
+		lcs = new LifeCycleStep();
+		lcs.name = "Detailed Design Plan";
+		lcs.defaultEffortCategory = lcs.getEffortCategory(effortCategories, "Plans");
+		lcs.defaultDeliverable = lcs.getDeliverable(deliverables, "Solution");
+		lifeCycleSteps.add(lcs);
+
+		lcs = new LifeCycleStep();
+		lcs.name = "Detailed Design/Prototype";
+		lcs.defaultEffortCategory = lcs.getEffortCategory(effortCategories, "Deliverables");
+		lcs.defaultDeliverable = lcs.getDeliverable(deliverables, "Conceptual Design");
+		lifeCycleSteps.add(lcs);
+
+		lcs = new LifeCycleStep();
+		lcs.name = "Detailed Design Review";
+		lcs.defaultEffortCategory = lcs.getEffortCategory(effortCategories, "Deliverables");
+		lcs.defaultDeliverable = lcs.getDeliverable(deliverables, "Conceptual Design");
+		lifeCycleSteps.add(lcs);
+
+		lcs = new LifeCycleStep();
+		lcs.name = "Implementation Plan";
+		lcs.defaultEffortCategory = lcs.getEffortCategory(effortCategories, "Plans");
+		lcs.defaultDeliverable = lcs.getDeliverable(deliverables, "Conceptual Design");
+		lifeCycleSteps.add(lcs);
+
+		lcs = new LifeCycleStep();
+		lcs.name = "Test Case Generation";
+		lcs.defaultEffortCategory = lcs.getEffortCategory(effortCategories, "Deliverables");
+		lcs.defaultDeliverable = lcs.getDeliverable(deliverables, "Test Cases");
+		lifeCycleSteps.add(lcs);
+
+		lcs = new LifeCycleStep();
+		lcs.name = "Solution Specification";
+		lcs.defaultEffortCategory = lcs.getEffortCategory(effortCategories, "Deliverables");
+		lcs.defaultDeliverable = lcs.getDeliverable(deliverables, "Solution");
+		lifeCycleSteps.add(lcs);
+
+		lcs = new LifeCycleStep();
+		lcs.name = "Solution Review";
+		lcs.defaultEffortCategory = lcs.getEffortCategory(effortCategories, "Deliverables");
+		lcs.defaultDeliverable = lcs.getDeliverable(deliverables, "Solution");
+		lifeCycleSteps.add(lcs);
+
+		lcs = new LifeCycleStep();
+		lcs.name = "Solution Implementation";
+		lcs.defaultEffortCategory = lcs.getEffortCategory(effortCategories, "Deliverables");
+		lcs.defaultDeliverable = lcs.getDeliverable(deliverables, "Solution");
+		lifeCycleSteps.add(lcs);
+
+		lcs = new LifeCycleStep();
+		lcs.name = "Unit/System Test";
+		lcs.defaultEffortCategory = lcs.getEffortCategory(effortCategories, "Deliverables");
+		lcs.defaultDeliverable = lcs.getDeliverable(deliverables, "Solution");
+		lifeCycleSteps.add(lcs);
+
+		lcs = new LifeCycleStep();
+		lcs.name = "Reflection";
+		lcs.defaultEffortCategory = lcs.getEffortCategory(effortCategories, "Deliverables");
+		lcs.defaultDeliverable = lcs.getDeliverable(deliverables, "Solution");
+		lifeCycleSteps.add(lcs);
+
+		lcs = new LifeCycleStep();
+		lcs.name = "Repository Update";
+		lcs.defaultEffortCategory = lcs.getEffortCategory(effortCategories, "Deliverables");
+		lcs.defaultDeliverable = lcs.getDeliverable(deliverables, "Solution");
+		lifeCycleSteps.add(lcs);
+
+		lcs = new LifeCycleStep();
+		lcs.name = "Planning";
+		lcs.defaultEffortCategory = lcs.getEffortCategory(effortCategories, "Plans");
+		lcs.defaultDeliverable = lcs.getDeliverable(deliverables, "Conceptual Design");
+		lifeCycleSteps.add(lcs);
+
+		lcs = new LifeCycleStep();
+		lcs.name = "Information Gathering";
+		lcs.defaultEffortCategory = lcs.getEffortCategory(effortCategories, "Deliverables");
+		lcs.defaultDeliverable = lcs.getDeliverable(deliverables, "Conceptual Design");
+		lifeCycleSteps.add(lcs);
+
+		lcs = new LifeCycleStep();
+		lcs.name = "Information Understanding";
+		lcs.defaultEffortCategory = lcs.getEffortCategory(effortCategories, "Deliverables");
+		lcs.defaultDeliverable = lcs.getDeliverable(deliverables, "Conceptual Design");
+		lifeCycleSteps.add(lcs);
+
+		lcs = new LifeCycleStep();
+		lcs.name = "Verifying";
+		lcs.defaultEffortCategory = lcs.getEffortCategory(effortCategories, "Deliverables");
+		lcs.defaultDeliverable = lcs.getDeliverable(deliverables, "Conceptual Design");
+		lifeCycleSteps.add(lcs);
+
+		lcs = new LifeCycleStep();
+		lcs.name = "Outlining";
+		lcs.defaultEffortCategory = lcs.getEffortCategory(effortCategories, "Deliverables");
+		lcs.defaultDeliverable = lcs.getDeliverable(deliverables, "Outline");
+		lifeCycleSteps.add(lcs);
+
+		lcs = new LifeCycleStep();
+		lcs.name = "Drafting";
+		lcs.defaultEffortCategory = lcs.getEffortCategory(effortCategories, "Deliverables");
+		lcs.defaultDeliverable = lcs.getDeliverable(deliverables, "Draft");
+		lifeCycleSteps.add(lcs);
+
+		lcs = new LifeCycleStep();
+		lcs.name = "Finalizing";
+		lcs.defaultEffortCategory = lcs.getEffortCategory(effortCategories, "Deliverables");
+		lcs.defaultDeliverable = lcs.getDeliverable(deliverables, "Report");
+		lifeCycleSteps.add(lcs);
+
+		lcs = new LifeCycleStep();
+		lcs.name = "Team Meeting";
+		lcs.defaultEffortCategory = lcs.getEffortCategory(effortCategories, "Deliverables");
+		lcs.defaultDeliverable = lcs.getDeliverable(deliverables, "Conceptual Design");
+		lifeCycleSteps.add(lcs);
+
+		lcs = new LifeCycleStep();
+		lcs.name = "Coach Meeting";
+		lcs.defaultEffortCategory = lcs.getEffortCategory(effortCategories, "Deliverables");
+		lcs.defaultDeliverable = lcs.getDeliverable(deliverables, "Conceptual Design");
+		lifeCycleSteps.add(lcs);
+
+		lcs = new LifeCycleStep();
+		lcs.name = "Stakeholder Meeting";
+		lcs.defaultEffortCategory = lcs.getEffortCategory(effortCategories, "Deliverables");
+		lcs.defaultDeliverable = lcs.getDeliverable(deliverables, "Conceptual Design");
+		lifeCycleSteps.add(lcs);
+		
+		//PROJECTS
+		Project project = new Project();
+		
+		project.name = "Business Project";
+		
+		project.lifeCycleSteps.add(project.getLifeCycleStep(lifeCycleSteps, "Planning"));
+		project.lifeCycleSteps.add(project.getLifeCycleStep(lifeCycleSteps, "Information Gathering"));
+		project.lifeCycleSteps.add(project.getLifeCycleStep(lifeCycleSteps, "Information Understanding"));
+		project.lifeCycleSteps.add(project.getLifeCycleStep(lifeCycleSteps, "Verifying"));
+		project.lifeCycleSteps.add(project.getLifeCycleStep(lifeCycleSteps, "Outlining"));
+		project.lifeCycleSteps.add(project.getLifeCycleStep(lifeCycleSteps, "Drafting"));
+		project.lifeCycleSteps.add(project.getLifeCycleStep(lifeCycleSteps, "Finalizing"));
+		project.lifeCycleSteps.add(project.getLifeCycleStep(lifeCycleSteps, "Team Meeting"));
+		project.lifeCycleSteps.add(project.getLifeCycleStep(lifeCycleSteps, "Coach Meeting"));
+		project.lifeCycleSteps.add(project.getLifeCycleStep(lifeCycleSteps, "Stakeholder Meeting"));
+		
+		projects.add(project);
+		
+		project = new Project();
+		project.name = "Development Project";
+		
+		project.lifeCycleSteps.add(project.getLifeCycleStep(lifeCycleSteps, "Problem Understanding"));
+		project.lifeCycleSteps.add(project.getLifeCycleStep(lifeCycleSteps, "Conceptual Design Plan"));
+		project.lifeCycleSteps.add(project.getLifeCycleStep(lifeCycleSteps, "Requirements"));
+		project.lifeCycleSteps.add(project.getLifeCycleStep(lifeCycleSteps, "Conceptual Design"));
+		project.lifeCycleSteps.add(project.getLifeCycleStep(lifeCycleSteps, "Conceptual Design Review"));
+		project.lifeCycleSteps.add(project.getLifeCycleStep(lifeCycleSteps, "Detailed Design Plan"));
+		project.lifeCycleSteps.add(project.getLifeCycleStep(lifeCycleSteps, "Detailed Design/Prototype"));
+		project.lifeCycleSteps.add(project.getLifeCycleStep(lifeCycleSteps, "Detailed Design Review"));
+		project.lifeCycleSteps.add(project.getLifeCycleStep(lifeCycleSteps, "Implementation Plan"));
+		project.lifeCycleSteps.add(project.getLifeCycleStep(lifeCycleSteps, "Test Case Generation"));
+		project.lifeCycleSteps.add(project.getLifeCycleStep(lifeCycleSteps, "Solution Specification"));
+		project.lifeCycleSteps.add(project.getLifeCycleStep(lifeCycleSteps, "Solution Review"));
+		project.lifeCycleSteps.add(project.getLifeCycleStep(lifeCycleSteps, "Solution Implementation"));
+		project.lifeCycleSteps.add(project.getLifeCycleStep(lifeCycleSteps, "Unit/System Test"));
+		project.lifeCycleSteps.add(project.getLifeCycleStep(lifeCycleSteps, "Reflection"));
+		project.lifeCycleSteps.add(project.getLifeCycleStep(lifeCycleSteps, "Repository Update"));
+		
+		projects.add(project);
+				
 	}
 	
 	List<Project> projects;
@@ -22,6 +377,72 @@ public class Definitions
 	List<Deliverable> deliverables;
 	List<Interruption> interruptions;
 	List<DefectCategory> defectCategories;
+	
+	public List<String> getProjectNames() 
+	{
+        List<String> names = new ArrayList<>();
+        for (Project project : projects) {
+            names.add(project.name);
+        }
+        return names;
+    }
+	
+	public List<String> getLifeCycleStepNames() 
+	{
+	       List<String> names = new ArrayList<>();
+	       for (LifeCycleStep step : lifeCycleSteps) 
+	       {
+	           names.add(step.name);
+	       }
+	       return names;
+	}
+	
+	public List<String> getEffortCategoryNames() 
+	{
+	        List<String> names = new ArrayList<>();
+	        for (EffortCategory category : effortCategories) {
+	            names.add(category.name);
+	        }
+	        return names;
+	}
+	 
+	public List<String> getPlanNames() 
+	{
+	        List<String> names = new ArrayList<>();
+	        for (Plan plan : plans) {
+	            names.add(plan.name);
+	        }
+	        return names;
+	}
+	 
+	 public List<String> getDeliverableNames() 
+	 {
+		 List<String> names = new ArrayList<>();
+	     for (Deliverable deliverable : deliverables) 
+	     {
+	    	 names.add(deliverable.name);
+	     }
+	     return names;
+	 }
+	 
+	 public List<String> getInterruptionNames() 
+	 	{
+	        List<String> names = new ArrayList<>();
+	        for (Interruption interruption : interruptions) {
+	            names.add(interruption.name);
+	        }
+	        return names;
+	    }
+	 
+	 public List<String> getDefectCategoryNames() 
+	 	{
+	        List<String> names = new ArrayList<>();
+	        for (DefectCategory category : defectCategories) {
+	            names.add(category.name);
+	        }
+	        return names;
+	    }
+
 }
 
 class Project
@@ -30,11 +451,24 @@ class Project
 	Project()
 	{
 		name = "";
-		lifeCycleSteps = null;
+		lifeCycleSteps = new ArrayList<>();;
 	}
 	
 	String name;
 	List<LifeCycleStep> lifeCycleSteps;
+	
+	public LifeCycleStep getLifeCycleStep(List<LifeCycleStep> lifeCycleSteps, String name)
+	{
+		for (LifeCycleStep lcs : lifeCycleSteps)
+		{
+			if (lcs.name == name)
+			{
+				return lcs;
+			}
+		}
+		
+		return null;
+	}
 }
 
 class LifeCycleStep
@@ -50,6 +484,32 @@ class LifeCycleStep
 	String name;
 	EffortCategory defaultEffortCategory;
 	Deliverable defaultDeliverable;
+	
+	public EffortCategory getEffortCategory(List<EffortCategory> effortCategories, String name)
+	{
+		for (EffortCategory ec : effortCategories)
+		{
+			if (ec.name == name)
+			{
+				return ec;
+			}
+		}
+		
+		return null;
+	}
+	
+	public Deliverable getDeliverable(List<Deliverable> deliverables, String name)
+	{
+		for (Deliverable deliverable : deliverables)
+		{
+			if (deliverable.name == name)
+			{
+				return deliverable;
+			}
+		}
+		
+		return null;
+	}
 }
 
 class EffortCategory
